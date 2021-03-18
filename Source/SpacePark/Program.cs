5418250b4b80f61b60196ce701d82a6a8147ed9b
PROGRAMMERINGEN\Patrik <patrikpersson45@gmail.com>
2021-03-18 14:15:14 +0100

    Patrik Test

--- Source/SpacePark/Program.cs
+++ Source/SpacePark/Program.cs
@@ -10,6 +10,10 @@ namespace SpacePark
             // Tjena tjabba hallå kära Jedis!
 
             // Testar//MAttias
+
+            /*Patrik Testar
+             var client = new RestClient("https://swapi.dev/api/");
+             var request = new RestRequest("people/", DataFormat.Json);*/
         }
     }
 }
