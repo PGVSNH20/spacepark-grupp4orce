e834fa21b46f7a9ba2779cad6cbe376acd9c8731
TooOldForThisGit <j.o.andersson@hotmail.se>
2021-03-18 14:11:50 +0100

    Main class

---
+++ Source/SpacePark/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SpacePark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Tjena tjabba hallå kära Jedis!
+        }
+    }
+}
