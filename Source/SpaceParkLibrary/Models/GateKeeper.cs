f21305ad675e657986b9e04a8a726f01d0e596f2
JMetropolis <jcityes@gmail.com>
2021-03-29 14:08:41 +0200

    gatekeeper

--- Source/SpaceParkLibrary/Models/GateKeeper.cs
+++ Source/SpaceParkLibrary/Models/GateKeeper.cs
@@ -8,6 +8,21 @@ namespace SpaceParkLibrary.Models
 {
     class GateKeeper
     {
+        private int starshipMaxLength = 200; //I meter
+
+        public bool StarshipLengthChecker(string inputShipName, int inputShipLength)
+		{
+			if (inputShipLength >= starshipMaxLength)
+			{
+				Console.WriteLine($"{inputShipName} är för stort! Välj en mindre!");
+				return false;
+			}
+			else
+			{
+				Console.WriteLine($"{inputShipName} får plats!");
+				return true;
+			}
+		}
         // GateKeepern är våran dörrvakt som släpper in godkända gäster,
         // och håller räkning på dem samt släpper ut dem ifall kreditvärdighet uppnås
         // för att sedan skicka till ekonomiavdelningen för fakturering
